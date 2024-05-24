865c91d02421f09fe50619eaec90b15d452c2b33
striveries <121800213+striveries@users.noreply.github.com>
2024-05-25 05:22:59 +0700

    base structures

--- striveries/settings.py
+++ striveries/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=q6v2&nm0yzb(8-w5-x11!pwntv272!234h+zr9r6lv=(-$mc*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
