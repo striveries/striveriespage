9ee1ae64a9af333391cb0739026530c91685611e
striveries <121800213+striveries@users.noreply.github.com>
2024-05-25 06:20:42 +0700

    adding tailwind ui

--- striveries/settings.py
+++ striveries/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
