090f84411b5cba2b0e4c4f014bbe13fbe1f72497
striveries <121800213+striveries@users.noreply.github.com>
2024-05-25 06:16:57 +0700

    adding tailwind ui

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -50,8 +50,8 @@ module.exports = {
          * for forms. If you don't like it or have own styling for forms,
          * comment the line below to disable '@tailwindcss/forms'.
          */
-        // require('@tailwindcss/forms'),
-        // require('@tailwindcss/typography'),
-        // require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 }
